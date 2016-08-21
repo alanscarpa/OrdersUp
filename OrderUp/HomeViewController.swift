d708fd57028dcf68e1bbe868b77a6b9232951654
Alan Scarpa <alan.scarpa@gmail.com>
2016-08-21 13:54:39 -0400

    Set up home vc

---
+++ OrderUp/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  OrderUp
+//
+//  Created by Alan Scarpa on 8/21/16.
+//  Copyright © 2016 ARC. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
